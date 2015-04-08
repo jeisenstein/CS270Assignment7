b26f20a119e63bbfa7bea40660a73ddecd24ce6c
jeisenstein <eisenste@unlv.nevada.edu>
2015-04-08 15:48:01 -0700

    CS270 Assignment 7
    Assignment 7, completed as instructed, database was migrated and
    application was tested on server prior to uploading onto github.

---
+++ app/models/roster.rb
@@ -0,0 +1,7 @@
+class Roster < ActiveRecord::Base
+  belongs_to :event
+  belongs_to :location
+  belongs_to :event
+  belongs_to :location
+  belongs_to :participant
+end
---
+++ db/migrate/20150408212325_create_rosters.rb
@@ -0,0 +1,10 @@
+class CreateRosters < ActiveRecord::Migration
+  def change
+    create_table :rosters do |t|
+      t.references :event, index: true
+      t.references :location, index: true
+
+      t.timestamps
+    end
+  end
+end
